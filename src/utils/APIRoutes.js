f6a3798d8669aa39c17ef46ca5d3bedbca3d6f05
surya <suryaprakashr1025@gmail.com>
2023-03-25 14:25:57 +0530

    push 25.3.2023

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:3333";
+// export const host = "http://localhost:3333";
+export const host = "https://chat-2bqa.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
